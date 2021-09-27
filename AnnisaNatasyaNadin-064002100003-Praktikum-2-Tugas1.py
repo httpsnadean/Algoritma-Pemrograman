8497caef89ab4622a6aebff0a85c7f221b2da7f5
httpsnadean <91054452+httpsnadean@users.noreply.github.com>
2021-09-27 14:12:28 +0700

    Praktikum 2 Tugas 1

---
+++ AnnisaNatasyaNadin-064002100003-Praktikum-2-Tugas1.py
@@ -0,0 +1,19 @@
+import math
+
+a = float(input("Masukkan nilai a: "))
+b = float(input("Masukkan nilai b: "))
+
+x = a + b
+print("Jumlah a dan b: ", x)
+x1 = a - b
+print("Selisih antara b dengan a: ", abs(x1))
+x2 = a * b
+print("Hasil kali a dan b: ", x2)
+x3 = a % b
+print("Sisa pembagian a dengan b: ", x3)
+x4 = a / b
+print("Pembagian a dengan b: ", x4)
+x5 = math.log10(a)
+print("Hasil dari log(a) : ", x5)
+x6 = a ** b
+print("a pangkat b: ", x6)
