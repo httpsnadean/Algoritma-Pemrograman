e2a12e53aeadd33b1a6919fe54aca800be3f9340
httpsnadean <91054452+httpsnadean@users.noreply.github.com>
2021-12-30 20:47:32 +0700

    Praktikum 12 Tugas 1

---
+++ AnnisaNatasyaNadin-064002100003-Praktikum-12-Tugas1.py
@@ -0,0 +1,57 @@
+print("Annisa Natasya Nadin\n064002100003\nPraktikum 12 Tugas 1")
+
+file = open('Negara.csv','r')
+
+class my_dictionary(dict):
+
+    def _init_(self):
+        self = dict()
+
+    def add(self, key, value):
+        self[key] = value
+
+data = my_dictionary()
+x1 = []
+x2 = []
+x3 = []
+x4 = []
+x5 = []
+
+x = 0
+for cache in file:
+    x += 1
+    cache = cache.split(',')
+    if x == 1:
+        pass
+    else:
+        p1 = cache[0]
+        x1.append(p1)
+        p2 = cache[1]
+        x2.append(p2)
+        p3 = cache[2]
+        x3.append(p3)
+        p4 = int(cache[3])
+        x4.append(p4)
+        p5 = int(cache[4])
+        x5.append(p5)
+
+data.add("Negara", x1)
+data.add("Ibukota", x2)
+data.add("Benua", x3)
+data.add("Luas", x4)
+data.add("Populasi", x5)
+
+
+import pandas as DATAFILE
+
+hasil = DATAFILE.DataFrame(data)
+
+print('\nLUAS DAN POPULASI DUNIA\n\n', hasil)
+
+
+mean = hasil.groupby(['Benua']).mean()
+std = hasil.groupby(['Benua']).std()
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print('\nRata-rata:\n', mean)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print('\nStandar Deviasi:\n', std)
